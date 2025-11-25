974f69f53859ffd504e33d3f6c07888cf7dbf389
michaelstucki <stcuthbertus@protonmail.com>
2025-11-25 14:41:42 -0700

    add rest controller for / endpoint

---
+++ src/main/java/com/michaelstucki/springbootdemo/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.michaelstucki.springbootdemo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // Expose "/" endpoint that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() { return "Hello World!"; }
+}
